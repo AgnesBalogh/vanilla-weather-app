216d5fa02616e364d0d913f083276d60717f753e
AgnesBalogh <63673667+AgnesBalogh@users.noreply.github.com>
2021-03-19 23:07:49 +0100

    Updated current location forecast

--- src/app.js
+++ src/app.js
@@ -84,10 +84,7 @@ function displayForecast(response) {
     `;
     }
 
-   
 
-
-    console.log();
 }
 
 function searchCity(city) {
@@ -117,6 +114,8 @@ function searchLocation(position) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
 
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 
@@ -129,6 +128,8 @@ function showCurrentLocation(event) {
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", showCurrentLocation);
+
+
 
 
 
